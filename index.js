374dcaa8e7ed49d0bf87067eeeb554c04a2e1c23
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-09-07 20:38:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Your code here
-function saturdayFun() {
- return "This Saturday, I want to ....!"
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
 }
+saturdayFun("bathe my dog"); 
 
-function mondayWork( go = "go to the office") {
-  return "This Monday, I will ... ."
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
 }
+mondayWork("work from home");
 
 function wrapAdjective(y='*') {
     return function(x='special') {
