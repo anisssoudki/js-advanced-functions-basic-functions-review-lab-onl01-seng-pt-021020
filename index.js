7ff901073f44bf25aeaf12f20a78e52c0409aab1
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-09-07 20:37:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,35 @@ function saturdayFun() {
 function mondayWork( go = "go to the office") {
   return "This Monday, I will ... ."
 }
+
+function wrapAdjective(y='*') {
+    return function(x='special') {
+        return `You are ${y}${x}${y}!`
+    }
+}
+
+const Calculator = {
+    add: function(a,b) {
+        return a +b;
+    },
+
+    subtract: function(a,b) {
+        return a-b;
+    },
+    multiply: function(a,b){
+        return a*b;
+    },
+
+    divide: function(a,b){
+        return a/b;
+    }
+}
+
+function actionApplyer(start, arr) {
+    let a = start;
+
+    for (let i = 0; i < arr.length; i++) {
+        a = arr[i](a)
+    }
+    return a
+}
